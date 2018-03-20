80d8dfdac29b82f6552f30b8fa78e4d89c8be7b1
Cristian Ponce <cristianrponce06@gmail.com>
2018-03-19 21:38:29 -0700

    Initial Project Setup
    * App Icon
    * UI Layout

---
+++ Doge Breeds/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Doge Breeds
+//
+//  Created by Cristian Ponce on 3/19/18.
+//  Copyright © 2018 Cristian Ponce. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
